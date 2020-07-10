fe3d3db4ee040072f852d516357b71b9b5c29d1f
Eugene White <eugenewhite1994@gmail.com>
2020-07-10 21:05:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,5 @@
-# def oxford_comma(array)
-# hello = ["k", "i", "w", "i"]
-# result = hello.join
-# end
-
-# oxford_comma("kiwi")
-
 def oxford_comma(array)
-joinedArray = array.join
-array.insert(4, "and Durian")
+hello = ["k", "i", "w", "i"]
+result = hello.join
 end
+
