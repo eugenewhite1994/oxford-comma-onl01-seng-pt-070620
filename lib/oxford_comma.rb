9f28d930e09143008351e9c1c440b94a26f11818
Eugene White <eugenewhite1994@gmail.com>
2020-07-10 21:34:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,3 +3,13 @@
 # return array.join(' and ') if array.size < 3
 # return array.join(' and ') if array.size < 4
 # end
+
+def oxford_comma(array)
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array[0..-2].join(', ') + ", and " + array[-1]
+  end
+end
